aff9742ca2c50ef265053eb498556f8f8d6b1a19
Geeobarros <ge.barros96@gmail.com>
2024-08-06 22:16:51 -0300

    adicionando template e rotas privadas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,23 +1,33 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Login  from "../pages/Login";
+import Login from "../pages/Login";
 import Cadastro from "../pages/Cadastro";
-import Layout from "./Layout";
+import { PrivateRouteLayout } from "../templates/Layout";
 import Dashboard from "../pages/Dashboard";
 import CadastrarLocais from "../pages/CadastrarLocais";
 import GerenciarLocais from "../pages/GerenciarLocais";
+import NotFound from "../pages/NotFound";
+import { useAuth } from "../context/authContext";
 
 export default function RoutesComponent() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login/>} />
-                <Route path="/cadastro" element={<Cadastro/>} />
-                <Route path="/dashboard" element={<Layout />}>
-                    <Route index element={<Dashboard />} />
-                    <Route index path="cadastrar-local" element={<CadastrarLocais />} />
-                    <Route path="gerenciar-locais" element={<GerenciarLocais />} />
-                </Route>
-            </Routes>
-        </Router>
-    )
+  const { usuario } = useAuth();
+
+  return (
+    <Router>
+      <Routes>
+        {!usuario ? (
+          <>
+            <Route path="/" element={<Login />}/>
+            <Route path="/cadastro" element={<Cadastro />} />
+          </>   
+        ) : (
+          <Route path="/" element={<PrivateRouteLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="cadastrar-local" element={<CadastrarLocais />} />
+            <Route path="gerenciar-locais" element={<GerenciarLocais />} />
+          </Route>
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
